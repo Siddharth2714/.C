a34de0182a973b3cd393a577c6f84af7feff8ac2
Siddharth2714 <92843916+Siddharth2714@users.noreply.github.com>
2021-10-25 11:47:30 +0530

    Add files via upload

---
+++ student details.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//variable declartion//
+	int age;
+	float cgpa;
+	char initial;
+	//initialisation//
+	age = 18;
+	cgpa = 8.5;
+	initial = 'R';
+	//OUTPUT//
+	printf("age of the is %d\n", age);
+	printf("cgpa of a student is %f\n", cgpa);
+	printf("initial of the student is %c\n", initial);
+}
